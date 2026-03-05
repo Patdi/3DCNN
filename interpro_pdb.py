d6d0ba5a8d1f962936d244ae07315539962314d2
Patdi <25618699+Patdi@users.noreply.github.com>
2026-03-04 16:43:33 -0800

    Update interpro_pdb.py

--- interpro_pdb.py
+++ interpro_pdb.py
@@ -4,14 +4,14 @@ import requests
 with open('accession.tsv', 'r') as file:
     for line in file:
         protein_id = line.strip("\n")
-        url = 'https://alphafold.ebi.ac.uk/files/AF-{protein_id}-F1-model_v6.pdb'
+        url = f"https://alphafold.ebi.ac.uk/files/AF-{protein_id}-F1-model_v6.pdb"
         try:
             response = requests.get(url, stream=True)
             pdb_filename = url.split("/")[-1]
             with open(pdb_filename, mode="wb") as pdbfile:
                 pdbfile.write(response.content)
-            with open('interpro_pdb_downloaded.txt') as downloaded:
-                donwloaded.write(pdb_filename)
+            with open('interpro_pdb_downloaded.txt', 'a') as downloaded:
+                donwloaded.write(protein_id + "\n")
         except Exception as e:
-            with open('interpro_pdb_not_downloaded.txt') as not_downloaded:
-                not_downloaded.write(pdb_filename)
+            with open('interpro_pdb_not_downloaded.txt', 'a') as not_downloaded:
+                not_downloaded.write(protein_id  + "\n")
