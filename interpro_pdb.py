96c091d6cb4e23a4525311a9a6a0d8d8effa4783
Patdi <25618699+Patdi@users.noreply.github.com>
2026-03-04 16:25:21 -0800

    Update interpro_pdb.py

--- interpro_pdb.py
+++ interpro_pdb.py
@@ -3,13 +3,15 @@
 import requests
 with open('accession.tsv', 'r') as file:
     for line in file:
-        #protein_id = line.strip("\n")
+        protein_id = line.strip("\n")
         url = 'https://alphafold.ebi.ac.uk/files/AF-{protein_id}-F1-model_v6.pdb'
         try:
-            # Copy a network object to a local file
             response = requests.get(url, stream=True)
             pdb_filename = url.split("/")[-1]
             with open(pdb_filename, mode="wb") as pdbfile:
                 pdbfile.write(response.content)
+            with open('interpro_pdb_downloaded.txt') as downloaded:
+                donwloaded.write(pdb_filename)
         except Exception as e:
-            print(f"An error occurred: {e}") #Write to a file the accesions that dont have a AF-PDB file
+            with open('interpro_pdb_not_downloaded.txt') as not_downloaded:
+                not_downloaded.write(pdb_filename)
